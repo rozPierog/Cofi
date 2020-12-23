d4166453a08bfff80443b5691c7ada1be000b599
Leon Omelan <rozpierog@gmail.com>
2020-12-23 20:14:14 +0100

    It starts to take up shape!

--- app/src/main/java/com/omelan/burr/components/RecipeItem.kt
+++ app/src/main/java/com/omelan/burr/components/RecipeItem.kt
@@ -1,37 +1,46 @@
 package com.omelan.burr.components
 
-import android.content.Context
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.imageResource
+
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.omelan.burr.R
+import com.omelan.burr.ui.BurrTheme
 
 @Composable
 fun RecipeItem(name: String, description: String, icon: Int) {
-//    Surface(border = BorderStroke(color = Color.Cyan, width = 2.dp), elevation = 15.dp, color = Color.LightGray) {
-        Column(modifier = Modifier.padding(15.dp)) {
-            Row(modifier = Modifier.fillMaxWidth().align(Alignment.Start)) {
-//                checkNotNull(icon)
-//                Image(imageResource(icon))
-                Text(text = name)
+    BurrTheme() {
+        Card(
+            elevation = 5.dp,
+            shape = RoundedCornerShape(15.dp),
+            modifier = Modifier.padding(10.dp)
+        )
+        {
+            Column(modifier = Modifier.padding(15.dp)) {
+                Row(modifier = Modifier.fillMaxWidth().align(Alignment.Start)) {
+//                    Image(imageResource(icon))
+                    Text(text = name)
+                }
+                Text(text = description)
             }
-            Text(text = description)
         }
-//    }
+    }
+
 }
 
 @Preview
