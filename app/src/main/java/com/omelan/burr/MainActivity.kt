1ef43cb30687247fb005b852457fa58cabcd4fce
Leon Omelan <rozpierog@gmail.com>
2020-12-30 17:43:02 +0100

    Full screen app

--- app/src/main/java/com/omelan/burr/MainActivity.kt
+++ app/src/main/java/com/omelan/burr/MainActivity.kt
@@ -3,7 +3,13 @@ package com.omelan.burr
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat.setOnApplyWindowInsetsListener
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -26,24 +32,44 @@ class MainActivity : AppCompatActivity() {
             Recipe(id = "2", name = "Ultimate French Press", description = "Hoffman"),
             Recipe(id = "3", name = "Ultimate Coś tam coś tam", description = "Hoffman"),
         )
-
+        var topPaddingInPx = 0
+        setOnApplyWindowInsetsListener(window.decorView) { _, insets ->
+            topPaddingInPx = insets.systemWindowInsetTop
+            insets.consumeSystemWindowInsets()
+        }
         setContent {
             val navController = rememberNavController()
+            Column {
 
-            NavHost(navController, startDestination = "list") {
-                composable("list") {
-                    RecipeList(recipes = listOfRecipes, navigateToRecipe = { recipeId ->
-                        navController.navigate("recipe/${recipeId}")
-                    })
-                }
-                composable("recipe/{recipeId}") { backStackEntry ->
-                    val recipeId = backStackEntry.arguments?.getString("recipeId")
-                    val pickedRecipe = listOfRecipes.find { it.id == recipeId }
-                        ?: throw IllegalArgumentException("No recipeId on transition!")
-                    RecipeTimerPage(pickedRecipe)
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(id = R.string.app_name))
+                    },
+                    backgroundColor = colorResource(id = R.color.navigationBar),
+                    contentColor = colorResource(id = R.color.textPrimary),
+                    modifier = Modifier.padding(
+                        top = (topPaddingInPx / (
+                                resources?.displayMetrics?.density
+                                    ?: 1f
+                                )).dp
+                    )
+                )
+                NavHost(navController, startDestination = "list") {
+                    composable("list") {
+                        RecipeList(recipes = listOfRecipes, navigateToRecipe = { recipeId ->
+                            navController.navigate("recipe/${recipeId}")
+                        })
+                    }
+                    composable("recipe/{recipeId}") { backStackEntry ->
+                        val recipeId = backStackEntry.arguments?.getString("recipeId")
+                        val pickedRecipe = listOfRecipes.find { it.id == recipeId }
+                            ?: throw IllegalArgumentException("No recipeId on transition!")
+                        RecipeTimerPage(pickedRecipe)
+                    }
                 }
             }
 
         }
     }
+
 }
