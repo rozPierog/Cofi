6cb2b0a0cd01fe7067e07f7cd959fc297526fa63
Leon Omelan <rozpierog@gmail.com>
2021-01-07 13:54:28 +0100

    [RecipeDetails] i18n

--- app/src/main/java/com/omelan/burr/pages/RecipeDetails.kt
+++ app/src/main/java/com/omelan/burr/pages/RecipeDetails.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -176,7 +177,13 @@ fun RecipeDetails(
                                         Icons.Rounded.PlayArrow
                                     }
                                 )
-                                Text(text = if (isAnimationRunning) "Pause" else "Start")
+                                Text(
+                                    text = if (isAnimationRunning) {
+                                        stringResource(id = R.string.recipe_details_button_pause)
+                                    } else {
+                                        stringResource(id = R.string.recipe_details_button_start)
+                                    }
+                                )
                             }
                             Spacer(modifier = Modifier.height(25.dp))
                         }
