85bdac22eedac4c8e170463742c48f24b7b64041
Leon Omelan <rozpierog@gmail.com>
2021-01-10 13:19:25 +0100

    DescriptionTest for expanding toggle

--- app/src/androidTest/java/com/omelan/cofi/components/DescriptionTest.kt
+++ app/src/androidTest/java/com/omelan/cofi/components/DescriptionTest.kt
@@ -1,8 +1,9 @@
 package com.omelan.cofi.components
 
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.state.ToggleableState
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
 import com.omelan.cofi.ui.CofiTheme
 import org.junit.Rule
 import org.junit.Test
@@ -19,11 +20,37 @@ class DescriptionTest {
     @Test
     fun shortDescriptionTest() {
         // Start the app
+        val testDescription = "Test description"
         composeTestRule.setContent {
             CofiTheme {
-                Description(descriptionText = "Test description")
+                Description(descriptionText = testDescription)
             }
         }
-        composeTestRule.onNodeWithText("Test description").assertIsDisplayed()
+        composeTestRule.onNodeWithText(testDescription).assertIsDisplayed()
+    }
+
+    @Test
+    fun expandingDescription() {
+        val testDescription =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
+                    "incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
+                    "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                    "consequat. Duis aute irure dolor in reprehenderit in voluptate velit " +
+                    "esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat " +
+                    "cupidatat non proident, sunt in culpa qui officia deserunt mollit anim " +
+                    "id est laborum."
+        composeTestRule.setContent {
+            CofiTheme {
+                Description(descriptionText = testDescription)
+            }
+        }
+        val descriptionNode = composeTestRule.onNodeWithText(testDescription)
+        descriptionNode.assertIsDisplayed().assertIsToggleable()
+        descriptionNode.assert(
+            SemanticsMatcher.expectValue(SemanticsProperties.ToggleableState, ToggleableState.Off)
+        )
+        descriptionNode.performClick().assert(
+            SemanticsMatcher.expectValue(SemanticsProperties.ToggleableState, ToggleableState.On)
+        )
     }
 }
