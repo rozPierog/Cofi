cba9dd2750ab080edaa5104ceadea256fbd58b90
Leon Omelan <rozpierog@gmail.com>
2021-07-23 12:58:18 +0200

    Text field capitalization

--- app/src/main/java/com/omelan/cofi/components/StepAddCard.kt
+++ app/src/main/java/com/omelan/cofi/components/StepAddCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -92,6 +93,7 @@ fun StepAddCard(
                     value = stepName.value,
                     singleLine = true,
                     onValueChange = { stepName.value = it },
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
                     modifier = Modifier
                         .testTag(
                             "step_name"
--- app/src/main/java/com/omelan/cofi/pages/RecipeEdit.kt
+++ app/src/main/java/com/omelan/cofi/pages/RecipeEdit.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -23,6 +24,7 @@ import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -185,6 +187,7 @@ fun RecipeEdit(
                                 .fillMaxWidth()
                                 .testTag("recipe_edit_name"),
                             singleLine = true,
+                            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
                             label = { Text(text = stringResource(id = R.string.recipe_edit_name)) },
                         )
                     }
@@ -197,6 +200,7 @@ fun RecipeEdit(
                             .fillMaxWidth()
                             .padding(bottom = spacingDefault)
                             .testTag("recipe_edit_description"),
+                        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
                         label = {
                             Text(text = stringResource(id = R.string.recipe_edit_description))
                         },
