61a81687f719ec579a8aeeeb8b7fb8d0d1fb2ba6
Leon Omelan <rozpierog@gmail.com>
2021-11-13 16:18:54 +0100

    Trying to wipe screenshots before creating new ones

--- app/src/debug/java/com/omelan/cofi/utils/ScreenshotsHelpers.kt
+++ app/src/debug/java/com/omelan/cofi/utils/ScreenshotsHelpers.kt
@@ -1,5 +1,6 @@
 package com.omelan.cofi.utils
 
+import android.content.ContentUris
 import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
@@ -27,11 +28,37 @@ object ScreenshotsHelpers {
 
         return runCatching {
             with(context.contentResolver) {
+                val selection = "${MediaStore.MediaColumns.RELATIVE_PATH}='Pictures/'"
+                query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    arrayOf(MediaStore.MediaColumns._ID, MediaStore.MediaColumns.DISPLAY_NAME),
+                    selection,
+                    null,
+                    null
+                ).use {
+                    if (it != null && it.count >= 1) {
+                        for (i in 0 until it.count) {
+                            it.moveToNext()
+                            val id =
+                                it.getLong(it.getColumnIndexOrThrow(MediaStore.MediaColumns._ID))
+                            val name =
+                                it.getString(it.getColumnIndexOrThrow(MediaStore.MediaColumns.DISPLAY_NAME))
+                            if (name == displayName) {
+                                val imageUri = ContentUris.withAppendedId(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    id
+                                )
+                                delete(imageUri, null, null)
+                            }
+                        }
+                    }
+                }
                 insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)?.also {
                     uri = it // Keep uri reference so it can be removed on failure
                     openOutputStream(it)?.use { stream ->
-                        if (!bitmap.compress(format, 95, stream))
+                        if (!bitmap.compress(format, 95, stream)) {
                             throw IOException("Failed to save bitmap.")
+                        }
                     } ?: throw IOException("Failed to open output stream.")
                 } ?: throw IOException("Failed to create new MediaStore record.")
             }
