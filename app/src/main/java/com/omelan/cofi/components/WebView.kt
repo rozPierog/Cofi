fed57c77dffd04fc250dc4f59795c58b781bd460
Lukas Neugebauer <25110308+lneugebauer@users.noreply.github.com>
2022-01-18 11:20:53 +0100

    Fix web view theming

--- app/src/main/java/com/omelan/cofi/components/WebView.kt
+++ app/src/main/java/com/omelan/cofi/components/WebView.kt
@@ -2,18 +2,36 @@ package com.omelan.cofi.components
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebSettingsCompat.FORCE_DARK_OFF
+import androidx.webkit.WebSettingsCompat.FORCE_DARK_ON
+import androidx.webkit.WebViewFeature
 
 @Composable
 fun WebView(urlToRender: String) {
+    val darkTheme = isSystemInDarkTheme()
     AndroidView(
         factory = { context ->
             val webView = WebView(context)
             webView.webViewClient = WebViewClient()
             webView.loadUrl(urlToRender)
             return@AndroidView webView
+        },
+        update = { webView ->
+            if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                if (darkTheme) {
+                    WebSettingsCompat.setForceDark(webView.settings, FORCE_DARK_ON)
+                } else {
+                    WebSettingsCompat.setForceDark(webView.settings, FORCE_DARK_OFF)
+                }
+            }
         }
     )
 }
