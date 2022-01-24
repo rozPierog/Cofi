a914a8a82c1ebf293c5fb4a315985ab10fae2a37
rozPierog <rozpierog@gmail.com>
2022-01-24 18:48:14 +0100

    Cleaner build of annotated string in licenses in DependencyItem

--- app/src/main/java/com/omelan/cofi/pages/settings/licenses/DependencyItem.kt
+++ app/src/main/java/com/omelan/cofi/pages/settings/licenses/DependencyItem.kt
@@ -11,8 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.omelan.cofi.pages.settings.settingsItemModifier
@@ -75,6 +76,7 @@ fun String.parseJsonToDependencyList(): List<Dependency> {
     return dependencyList
 }
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun DependencyItem(dependency: Dependency) {
     val uriHandler = LocalUriHandler.current
@@ -83,17 +85,26 @@ fun DependencyItem(dependency: Dependency) {
     } else {
         dependency.year
     }
-    val licenses =
-        buildAnnotatedString {
-            val licenses = dependency.licenses.joinToString { license -> license.license }
-            append(licenses)
-            var lastPosition = 0
-            dependency.licenses.forEach {
-                val positionOfUrl = licenses.indexOf(it.license, startIndex = lastPosition)
-                lastPosition = positionOfUrl
-                addLink(positionOfUrl = positionOfUrl, text = it.license, url = it.license_url)
+    val licenses = buildAnnotatedString {
+        dependency.licenses.forEachIndexed { index, license ->
+            withStyle(
+                SpanStyle(
+                    color = MaterialTheme.colorScheme.secondary,
+                    textDecoration = TextDecoration.Underline
+                )
+            ) {
+                withAnnotation(
+                    tag = URL_ANNOTATION,
+                    annotation = license.license_url,
+                ) {
+                    append(license.license)
+                }
+            }
+            if (index != dependency.licenses.size - 1) {
+                append(", ")
             }
         }
+    }
     Column(
         modifier = Modifier
             .settingsItemModifier(
@@ -146,22 +157,22 @@ fun DependencyPreview() {
         dependency = Dependency(
             project = "Nice package",
             description = "Contains Guava\\u0027s " +
-                "com.google.common.util.concurrent.ListenableFuture" +
-                " class,\\n    without any of its other classes -- but is also available in " +
-                "a second\\n    \\\"version\\\" that omits the class to avoid conflicts with " +
-                "the copy in Guava\\n    itself. The idea is:\\n\\n    - If users want only " +
-                "ListenableFuture, they depend on listenablefuture-1.0.\\n\\n    " +
-                "- If users want all of Guava, they depend on guava, which, as of Guava\\n   " +
-                " 27.0, depends on\\n   " +
-                " listenablefuture-9999.0-empty-to-avoid-conflict-with-guava. " +
-                "The 9999.0-...\\n    version number is enough for some build systems" +
-                " (notably, Gradle) to select\\n    that empty artifact over the " +
-                "\\\"real\\\" listenablefuture-1.0 -- avoiding a\\n    " +
-                "conflict with the copy of ListenableFuture in guava itself. If users are\\n " +
-                "   using an older version of Guava or a build system other than Gradle," +
-                " they\\n    may see class conflicts. If so, they can solve them by manually" +
-                " excluding\\n    the listenablefuture artifact or manually forcing their " +
-                "build systems to\\n    use 9999.0-....\",\n",
+                    "com.google.common.util.concurrent.ListenableFuture" +
+                    " class,\\n    without any of its other classes -- but is also available in " +
+                    "a second\\n    \\\"version\\\" that omits the class to avoid conflicts with " +
+                    "the copy in Guava\\n    itself. The idea is:\\n\\n    - If users want only " +
+                    "ListenableFuture, they depend on listenablefuture-1.0.\\n\\n    " +
+                    "- If users want all of Guava, they depend on guava, which, as of Guava\\n   " +
+                    " 27.0, depends on\\n   " +
+                    " listenablefuture-9999.0-empty-to-avoid-conflict-with-guava. " +
+                    "The 9999.0-...\\n    version number is enough for some build systems" +
+                    " (notably, Gradle) to select\\n    that empty artifact over the " +
+                    "\\\"real\\\" listenablefuture-1.0 -- avoiding a\\n    " +
+                    "conflict with the copy of ListenableFuture in guava itself. If users are\\n " +
+                    "   using an older version of Guava or a build system other than Gradle," +
+                    " they\\n    may see class conflicts. If so, they can solve them by manually" +
+                    " excluding\\n    the listenablefuture artifact or manually forcing their " +
+                    "build systems to\\n    use 9999.0-....\",\n",
             version = "3.2.1",
             developers = listOf("Leon Omelan"),
             url = "jsonObject.getString( url )",
