d1e7023d5c18472332e60187b220c7fed86c6607
rozPierog <rozpierog@gmail.com>
2022-01-24 18:17:44 +0100

    DependencyItem handle missing url

--- app/src/main/java/com/omelan/cofi/pages/settings/licenses/License.kt
+++ app/src/main/java/com/omelan/cofi/pages/settings/licenses/License.kt
@@ -28,7 +28,7 @@ class Dependency(
     val description: String,
     val version: String,
     val developers: List<String>,
-    val url: String,
+    val url: String?,
     val year: String?,
     val licenses: List<License>,
 )
@@ -78,7 +78,7 @@ fun String.parseJsonToDependencyList(): List<Dependency> {
 @Composable
 fun DependencyItem(dependency: Dependency) {
     val uriHandler = LocalUriHandler.current
-    val year = if (dependency.year == null || dependency.year == "null") {
+    val year = if (dependency.year.isNullOrBlank() || dependency.year == "null") {
         "20XX"
     } else {
         dependency.year
@@ -109,7 +109,12 @@ fun DependencyItem(dependency: Dependency) {
     Column(
         modifier = Modifier
             .settingsItemModifier(
-                onClick = { uriHandler.openUri(dependency.url) },
+                onClick = {
+                    uriHandler.openUri(
+                        dependency.url ?: dependency.licenses.first().license_url
+                    )
+                },
+                enabled = !dependency.url.isNullOrBlank() && dependency.url != "null",
                 unlimitedHeight = true
             )
             .padding(8.dp)
@@ -153,22 +158,22 @@ fun DependencyPreview() {
         dependency = Dependency(
             project = "Nice package",
             description = "Contains Guava\\u0027s " +
-                "com.google.common.util.concurrent.ListenableFuture" +
-                " class,\\n    without any of its other classes -- but is also available in " +
-                "a second\\n    \\\"version\\\" that omits the class to avoid conflicts with " +
-                "the copy in Guava\\n    itself. The idea is:\\n\\n    - If users want only " +
-                "ListenableFuture, they depend on listenablefuture-1.0.\\n\\n    " +
-                "- If users want all of Guava, they depend on guava, which, as of Guava\\n   " +
-                " 27.0, depends on\\n   " +
-                " listenablefuture-9999.0-empty-to-avoid-conflict-with-guava. " +
-                "The 9999.0-...\\n    version number is enough for some build systems" +
-                " (notably, Gradle) to select\\n    that empty artifact over the " +
-                "\\\"real\\\" listenablefuture-1.0 -- avoiding a\\n    " +
-                "conflict with the copy of ListenableFuture in guava itself. If users are\\n " +
-                "   using an older version of Guava or a build system other than Gradle," +
-                " they\\n    may see class conflicts. If so, they can solve them by manually" +
-                " excluding\\n    the listenablefuture artifact or manually forcing their " +
-                "build systems to\\n    use 9999.0-....\",\n",
+                    "com.google.common.util.concurrent.ListenableFuture" +
+                    " class,\\n    without any of its other classes -- but is also available in " +
+                    "a second\\n    \\\"version\\\" that omits the class to avoid conflicts with " +
+                    "the copy in Guava\\n    itself. The idea is:\\n\\n    - If users want only " +
+                    "ListenableFuture, they depend on listenablefuture-1.0.\\n\\n    " +
+                    "- If users want all of Guava, they depend on guava, which, as of Guava\\n   " +
+                    " 27.0, depends on\\n   " +
+                    " listenablefuture-9999.0-empty-to-avoid-conflict-with-guava. " +
+                    "The 9999.0-...\\n    version number is enough for some build systems" +
+                    " (notably, Gradle) to select\\n    that empty artifact over the " +
+                    "\\\"real\\\" listenablefuture-1.0 -- avoiding a\\n    " +
+                    "conflict with the copy of ListenableFuture in guava itself. If users are\\n " +
+                    "   using an older version of Guava or a build system other than Gradle," +
+                    " they\\n    may see class conflicts. If so, they can solve them by manually" +
+                    " excluding\\n    the listenablefuture artifact or manually forcing their " +
+                    "build systems to\\n    use 9999.0-....\",\n",
             version = "3.2.1",
             developers = listOf("Leon Omelan"),
             url = "jsonObject.getString( url )",
