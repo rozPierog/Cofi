d24295fd0715726926cc8eed426b577e6e053f99
oJankowiak <alealejankowiak@gmail.com>
2022-10-02 17:19:57 +0200

    Adding new Test Cases to Recipe Info Test Suite

--- app/src/androidTest/java/com/omelan/cofi/components/RecipeInfoTest.kt
+++ app/src/androidTest/java/com/omelan/cofi/components/RecipeInfoTest.kt
@@ -1,14 +1,14 @@
 package com.omelan.cofi.components
 
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import com.omelan.cofi.R
 import com.omelan.cofi.model.Step
 import com.omelan.cofi.model.StepType
 import com.omelan.cofi.ui.CofiTheme
 import com.omelan.cofi.utils.toMillis
+import com.omelan.cofi.utils.toStringDuration
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,7 +32,7 @@ class RecipeInfoTest {
                             time = 5.toMillis(),
                             type = StepType.ADD_COFFEE,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -57,7 +57,12 @@ class RecipeInfoTest {
                             time = 5.toMillis(),
                             type = StepType.WATER,
                         ),
-                    )
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_swirl),
+                            time = 5.toMillis(),
+                            type = StepType.OTHER,
+                        ),
+                    ),
                 )
             }
         }
@@ -84,7 +89,12 @@ class RecipeInfoTest {
                             time = null,
                             type = StepType.WATER,
                         ),
-                    )
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_swirl),
+                            time = null,
+                            type = StepType.OTHER,
+                        ),
+                    ),
                 )
             }
         }
@@ -111,7 +121,12 @@ class RecipeInfoTest {
                             time = 0.toMillis(),
                             type = StepType.WATER,
                         ),
-                    )
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_swirl),
+                            time = 0.toMillis(),
+                            type = StepType.OTHER,
+                        ),
+                    ),
                 )
             }
         }
@@ -132,7 +147,7 @@ class RecipeInfoTest {
                             time = 5.toMillis(),
                             type = StepType.ADD_COFFEE,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -152,7 +167,7 @@ class RecipeInfoTest {
                             time = 300.toMillis(),
                             type = StepType.ADD_COFFEE,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -172,7 +187,7 @@ class RecipeInfoTest {
                             time = 5.toMillis(),
                             type = StepType.WATER,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -192,7 +207,7 @@ class RecipeInfoTest {
                             time = 5.toMillis(),
                             type = StepType.WATER,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -212,7 +227,7 @@ class RecipeInfoTest {
                             time = null,
                             type = StepType.WATER,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -221,7 +236,7 @@ class RecipeInfoTest {
     }
 
     @Test
-    fun checkNullTime() {
+    fun checkNoTimeWater() {
         composeTestRule.setContent {
             CofiTheme {
                 RecipeInfo(
@@ -232,7 +247,7 @@ class RecipeInfoTest {
                             time = 0,
                             type = StepType.WATER,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -252,7 +267,7 @@ class RecipeInfoTest {
                             time = null,
                             type = StepType.ADD_COFFEE,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -272,7 +287,7 @@ class RecipeInfoTest {
                             time = 0.toMillis(),
                             type = StepType.ADD_COFFEE,
                         ),
-                    )
+                    ),
                 )
             }
         }
@@ -297,11 +312,93 @@ class RecipeInfoTest {
                             time = 35.toMillis(),
                             type = StepType.WAIT,
                         ),
-                    )
+                    ),
                 )
             }
         }
         composeTestRule.onNodeWithTag("recipe_info_time").assertIsDisplayed()
         composeTestRule.onNodeWithTag("recipe_info_coffee").assertIsDisplayed()
+    }
+
+    @Test
+    fun sumValueCoffeeAndTime() {
+        val value1 = 5
+        val value2 = 12
+        val time1 = 6
+        val time2 = 13
+        composeTestRule.setContent {
+            CofiTheme {
+                RecipeInfo(
+                    steps = listOf(
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_coffee),
+                            value = value1,
+                            time = time1.toMillis(),
+                            type = StepType.ADD_COFFEE,
+                        ),
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_coffee),
+                            value = value2,
+                            time = time2.toMillis(),
+                            type = StepType.ADD_COFFEE,
+                        ),
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_swirl),
+                            time = time2.toMillis(),
+                            type = StepType.OTHER,
+                        ),
+                    ),
+                )
+            }
+        }
+        composeTestRule.onNodeWithTag("recipe_info_time").assertIsDisplayed().onChildren()
+            .filterToOne(
+                hasTestTag("recipe_info_text"),
+            ).assertTextEquals((time1 + time2 + time2).toMillis().toStringDuration())
+        composeTestRule.onNodeWithTag("recipe_info_coffee").assertIsDisplayed().onChildren()
+            .filterToOne(
+                hasTestTag("recipe_info_text"),
+            ).assertTextEquals("${value1 + value2}g")
+    }
+
+    @Test
+    fun sumValueWaterAndTime() {
+        val value1 = 5
+        val value2 = 6
+        val time1 = 7
+        val time2 = 8
+        composeTestRule.setContent {
+            CofiTheme {
+                RecipeInfo(
+                    steps = listOf(
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_water),
+                            value = value1,
+                            time = time1.toMillis(),
+                            type = StepType.WATER,
+                        ),
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_water),
+                            value = value2,
+                            time = time2.toMillis(),
+                            type = StepType.WATER,
+                        ),
+                        Step(
+                            name = stringResource(R.string.prepopulate_step_swirl),
+                            time = time2.toMillis(),
+                            type = StepType.OTHER,
+                        ),
+                    ),
+                )
+            }
+        }
+        composeTestRule.onNodeWithTag("recipe_info_time").assertIsDisplayed().onChildren()
+            .filterToOne(
+                hasTestTag("recipe_info_text"),
+            ).assertTextEquals((time1 + time2 + time2).toMillis().toStringDuration())
+        composeTestRule.onNodeWithTag("recipe_info_water").assertIsDisplayed().onChildren()
+            .filterToOne(
+                hasTestTag("recipe_info_text"),
+            ).assertTextEquals("${value1 + value2}g")
     }
 }
