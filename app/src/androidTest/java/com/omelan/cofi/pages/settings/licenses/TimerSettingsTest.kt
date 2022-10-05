7d51efa148b751b7f142c8bc9e92873b29d3981f
oJankowiak <alealejankowiak@gmail.com>
2022-10-05 19:56:06 +0200

    Updating TimeSettingsTests.kt: - TC no 1 has been fixed - Cleaned up the code - Added switch state checking - Added tests for changing switch with ListItem

--- app/src/androidTest/java/com/omelan/cofi/pages/settings/licenses/TimerSettingsTest.kt
+++ app/src/androidTest/java/com/omelan/cofi/pages/settings/licenses/TimerSettingsTest.kt
@@ -3,6 +3,8 @@ package com.omelan.cofi.pages.settings.licenses
 import android.content.Context
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.omelan.cofi.LocalPiPState
@@ -32,8 +34,24 @@ class TimerSettingsTest {
         }
     }
 
+    private fun checkListItem(listItemTag: String, switchTag: String) {
+        val listItem = composeTestRule.onNodeWithTag(listItemTag)
+        val switch = composeTestRule.onNodeWithTag(switchTag)
+
+        listItem.assertIsDisplayed()
+        val currentSwitchState =
+            switch.fetchSemanticsNode().config[SemanticsProperties.ToggleableState]
+        if (currentSwitchState == ToggleableState.Off) {
+            switch.performClick()
+        }
+        switch.assertIsOn()
+        listItem.performClick()
+        switch.assertIsOff().performClick().assertIsOn()
+    }
+
     @Test
     fun showTimerSettings() {
+        renderTimerSettings()
         composeTestRule.onNodeWithTag("settings_timer_list_item_pip").assertIsDisplayed()
         composeTestRule.onNodeWithTag("settings_timer_list_item_sound").assertIsDisplayed()
         composeTestRule.onNodeWithTag("settings_timer_list_item_vibration").assertIsDisplayed()
@@ -43,28 +61,19 @@ class TimerSettingsTest {
     @Test
     fun checkPipSettings() {
         renderTimerSettings()
-        composeTestRule.onNodeWithTag("settings_timer_list_item_pip")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag("settings_timer_switch_pip")
-            .assertIsOn().performClick().assertIsOff()
+        checkListItem("settings_timer_list_item_pip", "settings_timer_switch_pip")
     }
 
     @Test
     fun checkSoundSettings() {
         renderTimerSettings()
-        composeTestRule.onNodeWithTag("settings_timer_list_item_sound")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag("settings_timer_switch_sound")
-            .assertIsOn().performClick().assertIsOff()
+        checkListItem("settings_timer_list_item_sound", "settings_timer_switch_sound")
     }
 
     @Test
     fun checkVibrationSettings() {
         renderTimerSettings()
-        composeTestRule.onNodeWithTag("settings_timer_list_item_vibration")
-            .assertIsDisplayed()
-        composeTestRule.onNodeWithTag("settings_timer_switch_vibration")
-            .assertIsOn().performClick().assertIsOff()
+        checkListItem("settings_timer_list_item_vibration", "settings_timer_switch_vibration")
     }
 
     @Test
