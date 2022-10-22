5dd392a6fe7f59ceff32d61ac83e4e8ee8bec457
rozPierog <rozpierog@gmail.com>
2022-10-22 19:42:43 +0200

    Change colors of Cofi Wear app

--- cofi-wearos/src/main/java/com/omelan/cofi/wearos/presentation/theme/Color.kt
+++ cofi-wearos/src/main/java/com/omelan/cofi/wearos/presentation/theme/Color.kt
@@ -2,20 +2,19 @@ package com.omelan.cofi.wearos.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 import androidx.wear.compose.material.Colors
+import com.omelan.cofi.share.brown200
+import com.omelan.cofi.share.brown700
+import com.omelan.cofi.share.iconBackground
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 val Red400 = Color(0xFFCF6679)
 
 internal val wearColorPalette: Colors = Colors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    secondaryVariant = Teal200,
+    primary = brown200,
+    primaryVariant = brown700,
+    secondary = iconBackground,
+    secondaryVariant = iconBackground,
     error = Red400,
     onPrimary = Color.Black,
-    onSecondary = Color.Black,
+    onSecondary = Color.White,
     onError = Color.Black
 )
