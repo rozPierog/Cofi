2a702573453ae6954da20409332b6a8c96f7f468
rozPierog <rozpierog@gmail.com>
2022-10-22 20:20:36 +0200

    cleanup ChannelHandler

--- cofi-wearos/src/main/java/com/omelan/cofi/wearos/presentation/ChannelHandler.kt
+++ cofi-wearos/src/main/java/com/omelan/cofi/wearos/presentation/ChannelHandler.kt
@@ -4,32 +4,35 @@ import com.google.android.gms.wearable.ChannelClient
 import com.omelan.cofi.share.model.AppDatabase
 import com.omelan.cofi.share.toRecipes
 import com.omelan.cofi.share.toSteps
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import org.json.JSONArray
 import java.nio.charset.StandardCharsets
 
 object ChannelHandler {
-    fun getDataFromChannel(db: AppDatabase, channelClient: ChannelClient, channel: ChannelClient.Channel) {
+    fun getDataFromChannel(
+        db: AppDatabase,
+        channelClient: ChannelClient,
+        channel: ChannelClient.Channel,
+    ) {
         val ioScope = CoroutineScope(Dispatchers.IO + Job())
         ioScope.launch {
-            val inputStream = channelClient.getInputStream(channel).await()
-            val jsonString = String(inputStream.readBytes(), StandardCharsets.UTF_8)
-            when (channel.path) {
-                "cofi/steps" -> {
-                    db.stepDao()
-                        .deleteAndCreate(JSONArray(jsonString).toSteps(true))
+            channelClient.getInputStream(channel).await().use {
+                val jsonString = String(it.readBytes(), StandardCharsets.UTF_8)
+                when (channel.path) {
+                    "cofi/steps" -> {
+                        db.stepDao()
+                            .deleteAndCreate(JSONArray(jsonString).toSteps(true))
+                    }
+                    "cofi/recipes" -> {
+                        db.recipeDao()
+                            .deleteAndCreate(JSONArray(jsonString).toRecipes(true))
+                    }
+                    else -> throw Exception("UNEXPECTED PATH ${channel.path}")
                 }
-
-                "cofi/recipes" -> {
-                    db.recipeDao()
-                        .deleteAndCreate(JSONArray(jsonString).toRecipes(true))
-                }
-
-                else -> throw Exception("UNEXPECTED PATH ${channel.path}")
-            }
-            withContext(Dispatchers.IO) {
-                inputStream.close()
             }
             channelClient.close(channel)
         }
