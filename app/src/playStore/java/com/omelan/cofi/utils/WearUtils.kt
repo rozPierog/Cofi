07ef24d627e252dad5174cf01cdf4473fe8b066b
rozPierog <rozpierog@gmail.com>
2022-10-24 16:42:13 +0200

    Cleanup WearUtils

--- app/src/playStore/java/com/omelan/cofi/utils/WearUtils.kt
+++ app/src/playStore/java/com/omelan/cofi/utils/WearUtils.kt
@@ -2,7 +2,7 @@ package com.omelan.cofi.utils
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
-import com.google.android.gms.tasks.Task
+import com.google.android.gms.wearable.ChannelClient
 import com.google.android.gms.wearable.Wearable
 import com.omelan.cofi.share.Recipe
 import com.omelan.cofi.share.Step
@@ -14,46 +14,45 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import org.json.JSONArray
-import java.io.IOException
-import java.io.OutputStream
 
 object WearUtils {
 
     private lateinit var recipesLiveData: LiveData<List<Recipe>>
     private lateinit var stepsLiveData: LiveData<List<Step>>
 
-    fun observeChangesAndSendToWear(activity: AppCompatActivity) {
-        val channelClient = Wearable.getChannelClient(activity)
-        val ioScope = CoroutineScope(Dispatchers.IO + Job())
-        val db = AppDatabase.getInstance(activity)
-        recipesLiveData = db.recipeDao().getAll()
-        stepsLiveData = db.stepDao().getAll()
-        fun sendDataToWearOS(data: List<SharedData>, channelName: String) {
-            ioScope.launch {
-                val nodes = Wearable.getNodeClient(activity).connectedNodes.await()
-                nodes.forEach { node ->
-                    val channel = channelClient.openChannel(node.id, "cofi/$channelName").await()
-                    val outputStreamTask: Task<OutputStream> =
-                        Wearable.getChannelClient(activity).getOutputStream(channel)
-                    outputStreamTask.addOnSuccessListener { outputStream ->
-                        try {
-                            val jsonArray = JSONArray()
-                            data.forEach { jsonArray.put(it.serialize()) }
-                            outputStream.write(jsonArray.toString().toByteArray())
-                            outputStream.flush()
-                            outputStream.close()
-                        } catch (e: IOException) {
-                            e.printStackTrace()
-                        }
+    private fun ChannelClient.sendDataToWearOS(
+        data: List<SharedData>,
+        channelName: String,
+        activity: AppCompatActivity,
+    ) {
+        CoroutineScope(Dispatchers.IO + Job()).launch {
+            val nodes = Wearable.getNodeClient(activity).connectedNodes.await()
+            nodes.forEach { node ->
+                val channel = openChannel(node.id, "cofi/$channelName").await()
+                val outputStreamTask = Wearable.getChannelClient(activity).getOutputStream(channel)
+                outputStreamTask.addOnSuccessListener { outputStream ->
+                    outputStream.use {
+                        val jsonArray = JSONArray()
+                        data.forEach { sharedData -> jsonArray.put(sharedData.serialize()) }
+                        it.write(jsonArray.toString().toByteArray())
+                        it.flush()
                     }
                 }
             }
         }
+    }
+
+    fun observeChangesAndSendToWear(activity: AppCompatActivity) {
+        val channelClient = Wearable.getChannelClient(activity)
+        val db = AppDatabase.getInstance(activity)
+        recipesLiveData = db.recipeDao().getAll()
+        stepsLiveData = db.stepDao().getAll()
+
         recipesLiveData.observe(activity) { recipes ->
-            sendDataToWearOS(recipes, "recipes")
+            channelClient.sendDataToWearOS(recipes, "recipes", activity)
         }
         stepsLiveData.observe(activity) { steps ->
-            sendDataToWearOS(steps, "steps")
+            channelClient.sendDataToWearOS(steps, "steps", activity)
         }
     }
 
