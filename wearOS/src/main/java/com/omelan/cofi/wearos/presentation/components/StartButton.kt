f85d5f4f7aa372a2f42503005704d2ebbe568963
rozPierog <rozpierog@gmail.com>
2023-02-28 07:23:04 +0100

    cleanup wearOS.StartButton

--- wearOS/src/main/java/com/omelan/cofi/wearos/presentation/components/StartButton.kt
+++ wearOS/src/main/java/com/omelan/cofi/wearos/presentation/components/StartButton.kt
@@ -1,36 +1,28 @@
 package com.omelan.cofi.wearos.presentation.components
 
-import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Icon
 import com.omelan.cofi.wearos.R
-import kotlinx.coroutines.launch
 
 @Composable
 fun StartButton(
     isTimerRunning: Boolean,
     onClick: () -> Unit,
 ) {
-    val animatedFabRadii = remember { Animatable(100f) }
-    var atEnd by remember { mutableStateOf(false) }
+    val animatedFabRadii by animateFloatAsState(
+        if (isTimerRunning) 28.0f else 100f,
+        tween(if (isTimerRunning) 300 else 500),
+    )
 
-    LaunchedEffect(isTimerRunning) {
-        launch {
-            atEnd = isTimerRunning
-        }
-        launch {
-            animatedFabRadii.animateTo(
-                if (isTimerRunning) 28.0f else 100f,
-                tween(if (isTimerRunning) 300 else 500),
-            )
-        }
-    }
     val icon = remember(isTimerRunning) {
         if (isTimerRunning) {
             R.drawable.ic_pause
@@ -41,7 +33,7 @@ fun StartButton(
     Button(
         modifier = Modifier.testTag("start_button"),
         onClick = onClick,
-        shape = RoundedCornerShape(animatedFabRadii.value),
+        shape = RoundedCornerShape(animatedFabRadii),
     ) {
         Icon(
             painter = painterResource(icon),
