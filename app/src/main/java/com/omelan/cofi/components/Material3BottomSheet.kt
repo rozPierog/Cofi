361f288965e2d95086e1e545a016d7a8d05beec2
rozPierog <rozpierog@gmail.com>
2023-05-19 13:42:01 +0200

    Material3BottomSheet fill screen with a scrim

--- app/src/main/java/com/omelan/cofi/components/Material3BottomSheet.kt
+++ app/src/main/java/com/omelan/cofi/components/Material3BottomSheet.kt
@@ -4,13 +4,19 @@ package com.omelan.cofi.components
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
@@ -32,12 +38,27 @@ fun Material3BottomSheet(
         animationSpec = tween(),
         label = "Bottom Sheet top radii",
     )
+    val scrimAlpha by animateFloatAsState(
+        targetValue = if (sheetState.targetValue == SheetValue.Hidden) 0f else 0.32f,
+        animationSpec = tween(),
+        label = "scrim alpha",
+    )
     val modalBottomSheetShape =
         RoundedCornerShape(topEnd = modalBottomSheetShapeDp, topStart = modalBottomSheetShapeDp)
-    ModalBottomSheet(
-        onDismissRequest = onDismissRequest,
-        shape = modalBottomSheetShape,
-        sheetState = sheetState,
-        content = content,
-    )
+    val scrimColor = MaterialTheme.colorScheme.scrim
+    Box(
+        Modifier.fillMaxSize().drawBehind {
+            drawRect(color = scrimColor, alpha = scrimAlpha)
+
+        },
+    ) {
+        ModalBottomSheet(
+            onDismissRequest = onDismissRequest,
+            shape = modalBottomSheetShape,
+            sheetState = sheetState,
+            content = content,
+            scrimColor = Color.Transparent,
+        )
+    }
+
 }
