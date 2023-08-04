3436ff5889353a08f43bc9dcc77acbb340fb9d3e
rozPierog <rozpierog@gmail.com>
2023-08-04 10:11:51 +0200

    Add Tea related icons. Thanks to the patronage of @SeriousBug

--- cofi-share/src/main/java/com/omelan/cofi/share/model/Recipe.kt
+++ cofi-share/src/main/java/com/omelan/cofi/share/model/Recipe.kt
@@ -25,6 +25,8 @@ enum class RecipeIcon(@DrawableRes val icon: Int, @StringRes val nameResId: Int)
     Siphon(R.drawable.recipe_icon_siphon, R.string.name_siphon),
     Bripe(R.drawable.recipe_icon_bripe, R.string.name_bripe),
     Cezve(R.drawable.recipe_icon_cezve, R.string.name_cezve),
+    Tea(R.drawable.recipe_icon_tea, R.string.name_tea),
+    Teapot(R.drawable.recipe_icon_teapot, R.string.name_teapot),
 }
 
 open class RecipeIconTypeConverter {
