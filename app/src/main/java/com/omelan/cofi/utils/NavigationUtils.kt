4d271e53160f5a9e8ebe853b892c910fa4fe24cd
rozPierog <rozpierog@gmail.com>
2024-04-26 09:21:37 +0200

    Improve navigation animation to look better with predictive back

--- app/src/main/java/com/omelan/cofi/utils/NavigationUtils.kt
+++ app/src/main/java/com/omelan/cofi/utils/NavigationUtils.kt
@@ -9,23 +9,21 @@ import androidx.compose.animation.fadeOut
 import androidx.navigation.NavBackStackEntry
 
 const val tweenDuration = 300
-private fun offsetCalculation(width: Int) = width / 8
+private fun offsetCalculation(width: Int) = width / 4
 fun AnimatedContentTransitionScope<NavBackStackEntry>.slideIn(
     towards: AnimatedContentTransitionScope.SlideDirection,
 ) =
     fadeIn(tween(tweenDuration, easing = LinearOutSlowInEasing)) +
-        slideIntoContainer(
-            towards,
-            animationSpec = tween(tweenDuration, easing = FastOutSlowInEasing),
-            initialOffset = { fullWidth -> -(offsetCalculation(fullWidth)) },
-        )
+            slideIntoContainer(
+                towards,
+                animationSpec = tween(tweenDuration, easing = FastOutSlowInEasing),
+                initialOffset = { fullWidth -> -(offsetCalculation(fullWidth)) },
+            )
 
 fun AnimatedContentTransitionScope<NavBackStackEntry>.slideOut(
     towards: AnimatedContentTransitionScope.SlideDirection,
-) =
-    fadeOut(tween(tweenDuration, easing = LinearOutSlowInEasing)) +
-        slideOutOfContainer(
-            towards,
-            animationSpec = tween(tweenDuration, easing = FastOutSlowInEasing),
-            targetOffset = { fullWidth -> offsetCalculation(fullWidth) },
-        )
+) = slideOutOfContainer(
+        towards,
+        animationSpec = tween(tweenDuration, easing = FastOutSlowInEasing),
+        targetOffset = { fullWidth -> offsetCalculation(fullWidth) },
+    ) + fadeOut(tween(tweenDuration, easing = LinearOutSlowInEasing))
